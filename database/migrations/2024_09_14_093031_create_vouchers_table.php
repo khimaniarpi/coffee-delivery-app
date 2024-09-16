da986e28bb2d42c08e430a2cfd094daad7f0ed3a
Arpi Khimani <khimaniarpi@gmail.com>
2024-09-16 17:09:05 +0530

    added foreign keys in the tables

---
+++ database/migrations/2024_09_14_093031_create_vouchers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('code')->unique();
+            $table->string('discount_type');
+            $table->decimal('discount_value', 8, 2);
+            $table->integer('max_uses');
+            $table->integer('uses')->default(0);
+            $table->dateTime('valid_from');
+            $table->dateTime('valid_until');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
